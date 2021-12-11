32ccf7cf129e558128019495031c2f5a51e6121e
Valério Pires <valeriotcu007@hotmail.com>
2021-12-11 15:45:52 -0300

    Validador de senhas com o uso de Expressões Regulares

---
+++ .NET/CSharp/Solução de Problemas/Valida-Requisitos-de-Senhas-com-ExpressãoRegular/App.ValidaSenhas/Solucao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace App.ValidaSenhas
+{
+    class Solucao
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                string senha = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(senha)) break;
+                Regex rx = new Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,32}$");
+                Match match = rx.Match(senha);
+                
+                Console.WriteLine("Senha {0}.", match.Success ? "valida": "invalida"); 
+            }  
+        }
+    }
+}
